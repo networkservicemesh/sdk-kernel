d22f5fb5fc434dde28c90a15e72e023f6f9203a8
Sergey Semenov <sergey.semenov@xored.com>
2020-07-23 12:19:38 +0700

    Handle error from next (move interface back)
    Signed-off-by: Sergey Semenov <sergey.semenov@xored.com>

--- pkg/kernel/networkservice/inject/server.go
+++ pkg/kernel/networkservice/inject/server.go
@@ -43,6 +43,7 @@ func NewServer() networkservice.NetworkServiceServer {
 }
 
 func (a *injectServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	connID := request.GetConnection().GetId()
 	mech := kernel.ToMechanism(request.GetConnection().GetMechanism())
 
 	/* Lock the OS thread so we don't accidentally switch namespaces */
@@ -68,11 +69,19 @@ func (a *injectServer) Request(ctx context.Context, request *networkservice.Netw
 
 	err = a.moveInterfaceToAnotherNamespace(ifaceName, forwarderNetNSHandle, clientNetNSHandle)
 	if err != nil {
-		return nil, errors.Wrapf(err, "Unable to move network interface %s into the Client's namespace", ifaceName)
+		return nil, errors.Wrapf(err, "Failed to move network interface %s into the Client's namespace", ifaceName)
 	}
-	log.Entry(ctx).Infof("Moved network interface %s into the Client's namespace for connection %s", ifaceName, request.GetConnection().GetId())
+	log.Entry(ctx).Infof("Moved network interface %s into the Client's namespace for connection %s", ifaceName, connID)
 
-	return next.Server(ctx).Request(ctx, request)
+	conn, err := next.Server(ctx).Request(ctx, request)
+	if err != nil {
+		errMovingBack := a.moveInterfaceToAnotherNamespace(ifaceName, clientNetNSHandle, forwarderNetNSHandle)
+		if errMovingBack != nil {
+			log.Entry(ctx).Infof("Failed to move network interface %s into the Forwarder's namespace for connection %s", ifaceName, connID)
+		}
+		log.Entry(ctx).Infof("Moved network interface %s into the Forwarder's namespace for connection %s", ifaceName, connID)
+	}
+	return conn, err
 }
 
 func (a *injectServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
