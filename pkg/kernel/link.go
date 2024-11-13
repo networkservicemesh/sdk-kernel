4e370fac61210fd6301f2ba1f30c2a6e7d24a44e
Vladislav Byrgazov <vladislav.byrgazov@xored.com>
2024-11-13 12:09:32 +0500

    Fix header lint error
    Signed-off-by: Vladislav Byrgazov <vladislav.byrgazov@xored.com>

--- pkg/kernel/link.go
+++ pkg/kernel/link.go
@@ -2,7 +2,7 @@
 //
 // Copyright (c) 2021-2022 Nordix Foundation.
 //
-// Copyright (c) 2022-2023 Cisco and/or its affiliates.
+// Copyright (c) 2022-2024 Cisco and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
